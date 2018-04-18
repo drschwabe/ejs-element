b27ce47c6f8daf9d9713e2584b2b2abd35411276
Jiafeng Wu <jiafengwu0301@gmail.com>
2018-04-18 16:38:20 -0600

    remove jquery dependency

--- ejs-element.js
+++ ejs-element.js
@@ -1,11 +1,10 @@
 const ejs = require('ejs')
 ejs.delimiter = '?'
 
-const $ = require('jquery'), 
-      _ = require('underscore')
+const _ = require('underscore')
 
-var ejsElem = { 
-  ent : null, 
+var ejsElem = {
+  ent : null,
   ents : [],
   init : (elementName, state, render) => {
     var elem = {
@@ -16,36 +15,36 @@ var ejsElem = {
     ejsElem.ents.push(elem)
     if(render) ejsElem.render(elementName)
   },
-  parse : (elementName) => { 
-    let element =  $(elementName)[0]  
-    let template = _.unescape(element.outerHTML) 
-    while(template.indexOf("<!--?") >= 0) { template = template.replace("<!--?", "<?")} 
-    while(template.indexOf("?-->") >= 0) { template = template.replace("?-->", "?>")}      
-    while(template.indexOf("-->") >= 0) { template = template.replace("-->", ">")}  
-    if(template.indexOf("<!--") >= 0) { 
-      template = template.replace("!>", "-->") 
-    } 
-    while(template.indexOf("<!--?-") >= 0) { template = template.replace("<!--?-", "<?") }  
-    while(template.indexOf("?-->") >= 0) { template = template.replace("?-->", "?>") }  
-    return _.unescape(template) 
-  }, 
-  render : (elementName, state) => { 
+  parse : (elementName) => {
+    let element = document.getElementsByTagName(elementName)[0]
+    let template = _.unescape(element.outerHTML)
+    while(template.indexOf("<!--?") >= 0) { template = template.replace("<!--?", "<?")}
+    while(template.indexOf("?-->") >= 0) { template = template.replace("?-->", "?>")}
+    while(template.indexOf("-->") >= 0) { template = template.replace("-->", ">")}
+    if(template.indexOf("<!--") >= 0) {
+      template = template.replace("!>", "-->")
+    }
+    while(template.indexOf("<!--?-") >= 0) { template = template.replace("<!--?-", "<?") }
+    while(template.indexOf("?-->") >= 0) { template = template.replace("?-->", "?>") }
+    return _.unescape(template)
+  },
+  render : (elementName, state) => {
     //debugger
     var elem = _.findWhere(ejsElem.ents, { name: elementName})
-    if(!state) state = elem //< If no state is provided, use the element itself:     
-    state.element = elem //< Reference to the element obj; so templates can use a generic 
+    if(!state) state = elem //< If no state is provided, use the element itself:
+    state.element = elem //< Reference to the element obj; so templates can use a generic
     //obj path to access the current element state ie: <? state.element ?>
-    var renderedTemplate = ejs.render(elem.template, { state: state }) 
-    $(elementName)[0].outerHTML = renderedTemplate   
-    $(elementName).removeClass('invisible')
-  }, 
+    var renderedTemplate = ejs.render(elem.template, { state: state })
+    document.getElementsByTagName(elementName)[0].outerHTML = renderedTemplate
+    document.getElementsByTagName(elementName)[0].classList.remove("invisible")
+  },
   renderAll : (state) => {
     ejsElem.ents.forEach((elem) => {
-      if(!state) state = elem 
+      if(!state) state = elem
       var renderedTemplate = ejs.render(elem.template, { state: state })
-      $(elem.name).each((index, elem) => {
-        elem.outerHTML = renderedTemplate
-      }).removeClass('invisible')
+      document.getElementsByTagName(elem.name).forEach((index,elem)=>{
+          elem.outerHTML = renderedTemplate
+      }).classList.remove("invisible")
     })
   }
 }
